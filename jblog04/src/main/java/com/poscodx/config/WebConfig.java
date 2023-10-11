1969467ead809ba4e422076ed8e1b26c1149d67e
김종혁 <notify9637@naver.com>
2023-10-11 20:30:53 +0900

    jblog04 #3 webconfig1

---
+++ jblog04/src/main/java/com/poscodx/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.poscodx.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.poscodx.config.web.MVCConfig;
+import com.poscodx.config.web.MessageSourceConfig;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan({"com.poscodx.jblog.controller", "com.posocdx.jblog.exception"})
+@Import({MVCConfig.class, MessageSourceConfig.class})
+public class WebConfig {
+
+}
